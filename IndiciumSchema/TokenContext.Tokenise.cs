4b60e5eb26f14c8b97e6ae418a76e056a8beb916
Muhammad Miftah <mamift@gmail.com>
2019-06-09 11:35:19 +1000

    Doesn't work. Committing for reference.

--- IndiciumSchema/TokenContext.Tokenise.cs
+++ IndiciumSchema/TokenContext.Tokenise.cs
@@ -29,31 +29,65 @@ namespace Indicium.Schemas
                 }
 
                 while (index <= line.Length) {
+                    bool didMatch = false;
                     foreach (var definedToken in tokenDefinitions) {
                         var regex = definedToken.GetMatcher();
                         var match = regex.Match(line, index);
-                        
+
                         // because we're iterating over all the token defs,
                         // a regex might match something but from the wrong starting index.
                         var matchIndex = match.Index != index;
                         if (!match.Success || matchIndex) {
                             continue; // continue searching
                         }
+
                         var token = new Token {
                             TypedValue = $"{{{definedToken.Id} = '{match.Value}'}}"
                         };
                         tokenList.Add(token);
                         index += match.Length - 1;
+                        didMatch = true;
                         Debug.WriteLine($"{token.TypedValue} @ {index}");
                     }
-
+                    /*var token = GetToken(line, ref index);
+                    tokenList.Add(token);*/
                     index++; // continue searching
                 }
 
                 stringBuilder.Append(line);
             }
 
+            Debug.WriteLine($"{tokenList.Count} tokens.");
             return tokenList;
+        }
+
+        protected Token GetToken(string line, ref int index)
+        {
+            var tokenDefinitions = Token.ToList();
+            Token theToken = null;
+            foreach (var definedToken in tokenDefinitions) {
+                var regex = definedToken.GetMatcher();
+                var substr = line.Substring(index);
+                var match = regex.Match(substr);
+
+                // because we're iterating over all the token defs,
+                // a regex might match something but from the wrong starting index.
+                var matchIndex = match.Index != index;
+                if (!match.Success || matchIndex) {
+                    continue; // continue searching
+                }
+
+                var token = new Token {
+                    TypedValue = $"{{{definedToken.Id} = '{match.Value}'}}"
+                };
+
+                index += match.Length - 1;
+                Debug.WriteLine($"{token.TypedValue} @ {index}");
+                theToken = token;
+                break;
+            }
+
+            return theToken;
         }
     }
 }
