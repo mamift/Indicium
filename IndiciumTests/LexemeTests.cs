2b5e340f82cd972789f833d20a3934fcafee1700
Muhammad Miftah <mamift@gmail.com>
2019-09-23 13:28:26 +1000

    Added more tests.

--- IndiciumTests/LexemeTests.cs
+++ IndiciumTests/LexemeTests.cs
@@ -7,18 +7,41 @@ namespace Indicium.Tests
     public class LexemeTests
     {
         [Test]
-        public void EqualityTest1()
+        public void EqualityTestById1()
         {
             var lex = new Lexeme() {
                 Id = "Test",
                 TypedValue = "_test"
             };
 
-            var undefined = Lexeme.Undefined;
+            Assert.IsFalse(lex == Lexeme.Undefined);
+        }
 
-            Assert.AreNotEqual(lex, undefined);
+        [Test]
+        public void EqualityTestById2()
+        {
+            var lex = new Lexeme() {
+                Id = "Undefined",
+                TypedValue = "_test"
+            };
 
-            Assert.IsFalse(lex == undefined);
+            Assert.IsTrue(lex == Lexeme.Undefined);
+        }
+
+        [Test]
+        public void EqualityTestByTypedValue()
+        {
+            var defaultLexeme = new Lexeme() {
+                Id = "Test",
+                TypedValue = "default"
+            };
+
+            var returnLexeme = new Lexeme() {
+                Id = "DefaultKeyword",
+                TypedValue = "default"
+            };
+
+            Assert.IsFalse(defaultLexeme == returnLexeme);
         }
     }
 }
